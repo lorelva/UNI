86aad3907ac2cebc43bc1606db9ebcdbf06313cb
LORENA VALLE <lore1268valle@gmail.com>
2022-10-11 19:24:25 -0500

    Hi there

---
+++ WEB_DEVELOPMENT/validaciones_C/js/validacion.js
@@ -0,0 +1,8 @@
+var cadena = "Programacion Web web web";
+
+    document.write(cadena.bold() + "<br>");
+    document.write(cadena.fontcolor("red")+"<br>")
+    document.write(cadena.fontcolor("mediumred")+"<br>")
+    document.write(cadena.strike()+"<br>")
+    document.write(cadena.big()+"<br>")
+
